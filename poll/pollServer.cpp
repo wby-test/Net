39b22b5d3c390d549ef98549c651a34438c41c50
wby0701 <2829926173@qq.com>
2019-08-20 18:35:46 +0800

    poll over

--- poll/pollServer.cpp
+++ poll/pollServer.cpp
@@ -16,15 +16,15 @@ int main(int argc, char **argv)
 {
     int listenFd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenFd == -1) {
-        std::cout  << "create socket FD error: " << strerror(errno) << std::endl;
+        std::cout  << "create socket FD error: " << strerror(errno)  << __LINE__ << std::endl;
         return -1;
     }
 
     //set listenfd to nonblock , when the fd is created;
     int oldSockFlags = fcntl(listenFd, F_GETFL, 0);
     int newSockFlags = oldSockFlags | O_NONBLOCK;
-    if (fcntl(listenFd, newSockFlags, 0) == -1) {
-        std::cout << "set the sock to nonblock error: " << strerror(errno) << std::endl;
+    if (fcntl(listenFd, F_SETFL, newSockFlags) == -1) {
+        std::cout << "set the sock to nonblock error: " << strerror(errno)  << __LINE__ << std::endl;
         close(listenFd);
         return -1;
     }
@@ -37,17 +37,17 @@ int main(int argc, char **argv)
 
     struct sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr(INADDR_ANY);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddr.sin_port = htons(3000);
 
     if (bind(listenFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-        std::cout << "bind error: " << strerror(errno) << std::endl;
+        std::cout << "bind error: " << strerror(errno)  << __LINE__ << std::endl;
         close(listenFd);
         return -1;
     }    
 
     if (listen(listenFd, SOMAXCONN) == -1) {
-        std::cout << "listen error: " << strerror(errno) << std::endl;
+        std::cout << "listen error: " << strerror(errno)  << __LINE__ << std::endl;
         close(listenFd);
         return -1;
     }
@@ -85,13 +85,71 @@ int main(int argc, char **argv)
 
                     int clientFd = accept(fds[i].fd, (struct sockaddr*)&clientAddr, &clientLen);
                     if(clientFd != -1) {
-                        int oldSockFlag = fcntl(clientFd, GET)
+                        int oldSockFlag = fcntl(clientFd, F_GETFL, 0);
+                        int newSockFlag = oldSockFlag | O_NONBLOCK;
+                        if (fcntl(clientFd, F_SETFL, newSockFlag) == -1) {
+                            close(clientFd);
+                            std::cout << "set nonblock clientFd error :" << strerror(errno) << std::endl;
+                        }
+                        else
+                        {
+                            pollfd client;
+                            client.fd = clientFd;
+                            client.events = POLLIN;
+                            client.revents = 0;
+                            fds.push_back(client);
+                            std::cout << "add a new connect fd to fds: " << clientFd << std::endl;
+                        }    
                     }
                 }
+                else
+                {
+                    char buf[64] = { 0 };
+                    int iRetRecv = recv(fds[i].fd, buf, 64, 0);
+                    if (iRetRecv <= 0) {
+                        if(errno != EINTR || errno != EWOULDBLOCK) {
+                            //error or perr closed connect
+                            for (auto iter = fds.begin(); iter != fds.end(); iter++) {
+                                if(fds[i].fd == iter->fd) {
+                                    std::cout << "closed invalid connection " << iter->fd << std::endl;
+                                    close(iter->fd);
+                                    iter->fd = INVALID_FD;
+                                    exist_invalid_fd = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        std::cout << "recv data: " << buf << std::endl;
+                    }     
+                }
+                
+            }
+
+            else if(fds[i].revents & POLLERR)
+            {
+                std::cout << "poll error" << std::endl;
+            }
+        }
+
+        if(exist_invalid_fd) {
+            for(auto iter = fds.begin(); iter != fds.end(); ++iter) {
+                if(iter->fd == INVALID_FD) {
+                    iter = fds.erase(iter);
+                }
+                else {
+                    ++iter;
+                }
+                
             }
         }
 
     }
 
+    for(auto iter = fds.begin(); iter != fds.end(); ++iter) {
+        close(iter->fd);
+    }
     return 0;
 }
